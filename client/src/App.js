40659a983cf4b6759c91320a1a260a7ed273e03a
ahmadalarif <44748381+ahmadalarif@users.noreply.github.com>
2020-02-05 21:16:41 +0300

    ahmed do the all

--- client/src/App.js
+++ client/src/App.js
@@ -1,75 +1,33 @@
-import React from 'react';
-import './App.css';
-import NavBar from "./Components/NavBar"
+import React from "react";
+import "./App.css";
+import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
-import {Card, CardGroup,} from 'react-bootstrap'
-import Creat from "./Components/Creat"
-import AllUsers from './AllUsers'
+import { Card, CardGroup } from "react-bootstrap";
+import Creat from "./Components/Creat";
+import AllUsers from "./AllUsers";
 import Usersing from "./usersing";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import AddQuetion from "./AddQusetion/AddQuestion";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { ShowAll } from "./Questions/ShowAll";
+import {ShowOne} from './Questions/ShowOne'
 function App() {
   return (
     <div>
-    <Router>
+      <Router>
+        <NavBar />
+        {/* <h1> Test your Information </h1> */}
 
-    <div className="App">
-       <NavBar />
-       <h1> Test your Information </h1>
-       <Footer/>
-    </div>
-    <CardGroup>
-{/* usersing */}
-     <Card>
-     
-      <Card.Body>
+        {/* Exams */}
+        <Route exact path="/Exams" render={props => <ShowAll {...props} />} />
+        <Route exact path="/Exams/:id" render={props => <ShowOne {...props} />} />
 
-        <Card.Title>User</Card.Title>
-        <Link to="/createUser">Create new User</Link>
-
-        <Card.Text>
-       
-        </Card.Text>
-      </Card.Body>
-    
-     </Card>
-    <Card>
-      <Card.Body>
-        <Card.Title>Exam</Card.Title>
-        <Link to="/create">Create new Exam</Link>
-        <Card.Text>
- 
-        </Card.Text>
-      </Card.Body>
-     
-    </Card>
-    <Card>
-      <Card.Body>
-        <Card.Title>Manage Users</Card.Title>
-        <Link to="/allUsers">All users</Link>
-
-        <Card.Text>
- 
-        </Card.Text>
-      </Card.Body>
- 
-     </Card>
-    </CardGroup>
-      <div>
-      <Route path="/allUsers" component={AllUsers} />
-
+        <Route path="/addQuestion" render={() => <AddQuetion />} />
+        <Route path="/allUsers" component={AllUsers} />
         <Route path="/createUser" component={Usersing} />
         <Route path="/create" component={Creat} />
-      </div>
-    </Router>
+      </Router>
     </div>
-  
-
   );
-
 }
 
 export default App;
